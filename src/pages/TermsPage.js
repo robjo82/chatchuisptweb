2b059347dba5c5bb34b7715055a60afb6354a084
Robin JOSEPH <rjrdutg@gmail.com>
2023-08-27 15:10:29 +0200

    Initial commit

---
+++ src/pages/TermsPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TermsPage() {
+    return (
+        <div>
+            <h1>Conditions générales</h1>
+            <p>Contenu des conditions générales ici...</p>
+        </div>
+    );
+}
+
+export default TermsPage;
