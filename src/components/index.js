7120fec2c0828019f6a8fb472586a0007b535451
Izaak Aubert-Mécibah <102653398+IzaakAM@users.noreply.github.com>
2023-09-23 16:58:34 +0200

    feat: first user interface (#3)
    * feat: initialisation & structure
    
    * feat: navbar with responsive menu
    
    * feat: Hero V1 + Footer file
    
    * fix: better apsect of hero part
    
    * feat(hero): add parapgraph
    
    * feat: add CGU & Who_are_we files
    
    * fix(hero): update the description
    
    * ci: add Husky and Commitlint for commit formatting and hooks (#2)
    
    - Install Husky for Git hooks
    - Install Commitlint to enforce commit message format
    
    * fix: rebase issues
    
    * feat(AboutUs): Add v1
    
    * fix(hero and AboutUs): more responsive
    
    * fix(AboutUs): Picture of Robin sizes
    
    * fix(scripts): Added test script, mainly for the CI.
    
    * fix(index.css): unknown proprety
    
    ---------
    
    Co-authored-by: Robin JOSEPH <49043100+robjo82@users.noreply.github.com>
    Co-authored-by: robjo82 <rjrdutg@gmail.com>

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Hero from "./Hero";
+import AboutUs from "./AboutUs";
+import CGU from "./CGU";
+import Footer from "./Footer";
+
+
+export { Navbar, Hero, AboutUs, CGU, Footer };
